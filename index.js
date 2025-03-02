1cf3ec4d6104330d25dc984f282886d1ffcdd752
Michael <michaelbrian88@gmail.com>
2025-03-02 22:06:58 +0100

    connecting app & api initial config

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json({extended: true}))
 app.use(express.urlencoded({extended: true}))
-app.use(cors({credentials: true, origin: "http://localhost:3000"}))
+app.use(cors({credentials: true, origin: "http://localhost:5173"}))
 app.use(upload())
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
@@ -22,5 +22,5 @@ app.use('/api/posts' , postRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
-connect(process.env.MONGO_URI).then(app.listen(5000, () => console.log(`Server running on port ${process.env.PORT}`))).catch(error => {console.log(error)})
+connect(process.env.MONGO_URI).then(app.listen(process.env.PORT || 5000 , () => console.log(`Server running on port ${process.env.PORT}`))).catch(error => {console.log(error)})
 
